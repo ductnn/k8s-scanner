ad041aebfa3ce9e7dbaf837aec40616afc795089
ductnn <trannhuduc531998@gmail.com>
2025-11-09 21:51:00 +0700

    chore: increase limit restart
    Signed-off-by: ductnn <trannhuduc531998@gmail.com>

--- pkg/scanner/pod_restart.go
+++ pkg/scanner/pod_restart.go
@@ -1,6 +1,6 @@
 package scanner
 
-const RestartThreshold int32 = 5 // restart > 5 coi như bất thường
+const RestartThreshold int32 = 10 // restart > 10 coi như bất thường
 
 func CheckRestartSeverity(count int32, threshold int32) string {
 	if count > threshold {
